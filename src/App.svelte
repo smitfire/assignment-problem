48758f5b79f8eb3073554ddf16724c8941ecf85f
Nicolaas Smit <nick.smit@rtk.io>
2020-03-11 07:11:28 +0100

    udemy scetle assignment 1

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Goal from "./Goal.Svelte.svelte"
+					
+  let goal;
+</script>
+
+<input type="text" bind:value={goal} />
+<Goal goal={goal} />
---
+++ src/Goal.Svelte.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let goal = 'placeholder';
+
+    $: hasExclamationMark = goal.includes("!");
+</script>
+
+<style>
+  .red {
+    color: red;
+  }
+</style>
+
+<h1 class={hasExclamationMark ? 'red' : 'normal'}>{goal}</h1>
